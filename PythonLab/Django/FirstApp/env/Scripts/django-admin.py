f3c830b1a15145ea8807904e5f821f33074c60b8
Kaifee Mohammad <kaifee.mohammad@outlook.com>
2020-02-15 20:00:02 +0000

    python venv setup;djanog firstapp;

---
+++ PythonLab/Django/FirstApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\github\kaifee-mohammad\labs\pythonlab\django\firstapp\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
